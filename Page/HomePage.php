bbef4ddba99ac275545a52fe5e7ffd63b9093b33
Sarah Rawlinson <srrawlinson@hotmail.co.uk>
2022-08-27 22:38:16 +0100

    prettier look
    worked on css

--- Page/HomePage.php
+++ Page/HomePage.php
@@ -13,20 +13,15 @@ $name = "Sarah Rawlinson";
 <head><title><?php echo $name."'s Home Page" ?></title><br>
     <link rel="stylesheet" href="../Assets/CSS/HomePage.css" type="text/css">
 </head>
-<h1><?php echo $name."'s Home Page" ?></h1>
+
 <body>
-<div id="Header">
-</div>
-<div id="Body">
-    <form method="post" action="MyGames.php">
-        <div>
-            <h1></h1><label>Links</label><br><br></h1>
-            <a href="<?php echo $youTubeLink ?>">YouTube</a><br><br>
-            <a href="<?php echo $itchLink ?>">itch.io</a><br><br>
-            <a href="<?php echo $gitHubLink ?>">GitHub</a><br><br>
-        </div>
-    </form>
-</div>
+<h1><?php echo $name."'s Home Page" ?></h1>
+
+    <label>Links</label><br><br></h1>
+    <a href="<?php echo $youTubeLink ?>">YouTube</a><br><br>
+    <a href="<?php echo $itchLink ?>">itch.io</a><br><br>
+    <a href="<?php echo $gitHubLink ?>">GitHub</a><br><br>
+
 <h2><a href="MyGames.php">My Games</a></h2>
 
 </body>
