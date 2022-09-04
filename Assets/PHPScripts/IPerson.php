ecc33277b1f5cfd97631953a6501bede92b09330
Sarah Rawlinson <srrawlinson@hotmail.co.uk>
2022-09-04 21:26:33 +0100

    Person Class & IPerson

---
+++ Assets/PHPScripts/IPerson.php
@@ -0,0 +1,19 @@
+<?php
+
+interface IPerson
+{
+    public function GetTile() : string;
+    public function GetName(): string;
+    public function GetFirstName() : string;
+    public function GetLastName() : string;
+    public function GetEmail() : string;
+    public function GetAddressNumber() : string;
+    public function GetAddressStreetName() : string;
+    public function GetAddressCity() : string;
+    public function GetAddressRegion() : string;
+    public function GetAddressCountry() : string;
+    public function GetAge() : int;
+    public function GetDateOfBirth() : DateTime;
+    public function GetGender() : string;
+    public function GetUserName() : string;
+}
